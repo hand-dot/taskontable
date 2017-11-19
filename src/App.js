0a2c77adbd95dee22c18db3a401166820eb333d8
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-11-19 12:20:39 +0900

    tmp creating layout

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
-import * as firebase from 'firebase';
-import React, { Component } from 'react';
-import Button from 'material-ui/Button'
-import Handsontable from 'handsontable';
-import firebaseConf from './conf/firebase';
-import logo from './logo.svg';
-import './App.css';
-import 'handsontable/dist/handsontable.full.css';
+import * as firebase from "firebase";
+import React, { Component } from "react";
+
+import Grid from "material-ui/Grid";
+
+import Handsontable from "handsontable";
+import firebaseConf from "./conf/firebase";
+import "./App.css";
+import "handsontable/dist/handsontable.full.css";
 
 class App extends Component {
-  componentWillMount(){
-    let app = firebase.initializeApp({});
+  componentWillMount() {
+    firebase.initializeApp(firebaseConf);
+    let starCountRef = firebase.database().ref("/test/msg");
+    starCountRef.on("value", snapshot => {
+      console.log(snapshot.val());
+    });
   }
   componentDidMount() {
-    let hot = new Handsontable(document.getElementById('hot'), {});
+    let hot = new Handsontable(document.getElementById("hot"), {
+      stretchH: "all"
+    });
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div id="hot" />
-        <Button raised color="primary">
-          Hello World
-        </Button>
+        <div>
+          <Grid container>
+            <Grid item xs={12} sm={6}>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+            </Grid>
+            <Grid item xs={12}>
+              <div id="hot" />
+            </Grid>
+          </Grid>
+        </div>
       </div>
     );
   }
