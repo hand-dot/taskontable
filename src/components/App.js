0a882bfd7dc7dd3aa908618b05725c89cbfe5430
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-11-20 01:38:17 +0900

    add hotconfig

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,61 @@ class App extends Component {
   }
   componentDidMount() {
     let hot = new Handsontable(document.getElementById("hot"), {
-      stretchH: "all"
+      stretchH: "all",
+      rowHeaders: true,
+      manualRowMove: true,
+      colWidths: Math.round(window.innerWidth / 9),
+      columns: [
+        {
+          title: "済",
+          data: "done",
+          type: "checkbox",
+          colWidths: 30,
+          readOnly: true,
+          className: "htCenter htMiddle"
+        },
+        {
+          title: "カテゴリ",
+          data: "category",
+          type: "text"
+        },
+        {
+          title: "タイトル",
+          data: "title",
+          type: "text"
+        },
+        {
+          title: "見積(分)",
+          data: "estimate",
+          type: "numeric"
+        },
+        {
+          title: "実績(分)",
+          data: "actually",
+          type: "numeric",
+          readOnly: true
+        },
+        {
+          title: "開始時刻",
+          type: "time",
+          timeFormat: "hh:mm a",
+          correctFormat: true
+        },
+        {
+          title: "終了時刻",
+          type: "time",
+          timeFormat: "hh:mm a",
+          correctFormat: true
+        },
+        {
+          title: "備考",
+          type: "text"
+        },
+        {
+          title: "感想",
+          type: "text"
+        }
+      ]
     });
   }
   render() {
