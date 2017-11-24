5558f47027d10adde6e87d27b5f5ebd530ae33a6
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-11-25 01:52:03 +0900

    add contextmenu

--- src/confings/hot.js
+++ src/confings/hot.js
@@ -75,6 +75,26 @@ export default {
   comments: true,
   rowHeaders: true,
   manualRowMove: true,
+  contextMenu: {
+    items: {
+      row_above: {
+        name: '上に行を追加する',                
+        disabled() {
+          return this.getSelected()[0] === 0;
+        },
+      },
+      row_below: {
+        name: '下に行を追加する',        
+      },
+      hsep1: '---------',
+      remove_row: {
+        name: '行を削除する',
+        disabled() {
+          return this.getSelected()[0] === 0;
+        },
+      },
+    },
+  },
   colWidths: Math.round(window.innerWidth / 9),
   columns,
   cell: [
