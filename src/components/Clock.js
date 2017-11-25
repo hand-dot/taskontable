78464b0d15bd5878a546591084e23fe3f6b5aa86
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-11-26 04:42:56 +0900

    remove clock.css

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,19 +1,55 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import moment from 'moment';
 import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
-import '../styles/Clock.css';
 
-const styles = theme => ({
-  container: {
-    display: 'flex',
-    flexWrap: 'wrap',
+const clockHands = {
+  width: 0,
+  height: 0,
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transformOrigin: '50% 100%',
+  background: '#000',
+};
+
+const styles = () => ({
+  circle: {
+    width: 130,
+    height: 130,
+    margin: '0 auto',
+    position: 'relative',
+    border: '8px solid #000',
+    borderRadius: '50%',
   },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200,
+  face: {
+    '&:after': {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      width: 12,
+      height: 12,
+      margin: '-6px 0 0 -6px',
+      background: '#000',
+      borderRadius: 6,
+      content: '""',
+      display: 'block',
+    },
   },
+  hour: Object.assign({
+    padding: '4px 0 4px 25%',
+    borderRadius: '4px 0 0 4px',
+  }, clockHands),
+  minute: Object.assign({
+    margin: '-40% -3px 0',
+    padding: '40% 3px 0',
+    borderRadius: '3px 3px 0 0',
+  }, clockHands),
+  second: Object.assign({
+    margin: '-40% -1px 0 0',
+    padding: '40% 1px 0',
+  }, clockHands),
 });
 
 class Clock extends Component {
@@ -53,17 +89,21 @@ class Clock extends Component {
         <Typography gutterBottom type="subheading">
                 現在時刻
         </Typography>
-        <div className="hero-circle">
-          <div className="hero-face">
-            <div id="hour" className="hero-hour" />
-            <div id="minute" className="hero-minute" />
-            <div id="second" className="hero-second" />
+        <div className={this.props.classes.circle}>
+          <div className={this.props.classes.face}>
+            <div id="hour" className={this.props.classes.hour} />
+            <div id="minute" className={this.props.classes.minute} />
+            <div id="second" className={this.props.classes.second} />
           </div>
         </div>
-        <Typography type="display2" align="center">{`${('00' + this.state.hour).slice(-2) }:${ ('00' + this.state.minute).slice(-2)}`}</Typography>
+        <Typography type="display2" align="center">{`${(`00${this.state.hour}`).slice(-2)}:${(`00${this.state.minute}`).slice(-2)}`}</Typography>
       </div>
     );
   }
 }
+
+Clock.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 export default withStyles(styles)(Clock);
