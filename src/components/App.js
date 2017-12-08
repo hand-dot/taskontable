d0bb1275d84ea1cdba171a784afd650dc05ef6ac
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-12-09 05:28:59 +0900

    split hot controll section

--- src/components/App.js
+++ src/components/App.js
@@ -9,19 +9,15 @@ import 'handsontable/dist/handsontable.full.css';
 
 import Typography from 'material-ui/Typography';
 import Grid from 'material-ui/Grid';
-import Button from 'material-ui/Button';
-import SaveIcon from 'material-ui-icons/Save';
-import AddIcon from 'material-ui-icons/Add';
-import Switch from 'material-ui/Switch';
-import { FormControlLabel, FormGroup } from 'material-ui/Form';
+
 import Card, { CardContent } from 'material-ui/Card';
 import { LinearProgress } from 'material-ui/Progress';
-import Tooltip from 'material-ui/Tooltip';
 
 import { withStyles } from 'material-ui/styles';
 
-import Dashboad from './Dashboad';
 import GlobalHeader from './GlobalHeader';
+import Dashboad from './Dashboad';
+import TaskListCtl from './TaskListCtl';
 
 import firebaseConf from '../confings/firebase';
 import { hotConf, emptyHotData } from '../confings/hot';
@@ -51,12 +47,6 @@ const NotificationClone = (() => ('Notification' in window ? cloneDeep(Notificat
 firebase.initializeApp(firebaseConf);
 
 let hot = null;
-
-function addTask() {
-  if (hot) {
-    hot.alter('insert_row');
-  }
-}
 
 class App extends Component {
   constructor(props) {
@@ -274,56 +264,12 @@ class App extends Component {
                   {this.state.date.replace(/-/g, '/')} のタスク一覧
                 </Typography>
                 <Grid container spacing={5}>
-                  <Grid item xs={6}>
-                    <Typography type="caption" gutterBottom>
-                         *終了通知の予約を行うには見積を入力したタスクの開始時刻を入力してください。
-                    </Typography>
-                    <Typography type="caption" gutterBottom>
-                         *通知予約されたタスクの開始時刻に <i className="fa fa-clock-o fa-lg" /> が表示されます。(マウスホバーで予約時刻)
-                    </Typography>
-                    <Typography type="caption" gutterBottom>
-                        *開始時刻を削除、もしくは終了を入力すると終了通知の予約は削除されます。
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <Typography type="caption" gutterBottom>
-                      *セル上で右クリックすると現在時刻の入力・行の追加・削除を行えます。
-                    </Typography>
-                    <Typography type="caption" gutterBottom>
-                      *行を選択しドラッグアンドドロップでタスクを入れ替えることができます。
-                    </Typography>
-                    <Typography type="caption" gutterBottom>
-                        *列ヘッダーにマウスホバーすると各列の説明を見ることができます。
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <FormGroup>
-                      <FormControlLabel
-                        control={
-                          <Switch
-                            disabled={!('Notification' in window)}
-                            checked={this.state.notifiable}
-                            onChange={this.toggleNotifiable.bind(this)}
-                          />
-                        }
-                        label={`開始したタスクの終了時刻通知${!('Notification' in window) ? '(ブラウザが未対応です。)' : ''}`}
-                      />
-                    </FormGroup>
-                  </Grid>
-                  <Grid item xs={6}>
-                    <div style={{ textAlign: 'right' }}>
-                      <Button raised onClick={addTask} color="default">
-                        <AddIcon />
-                          追加
-                      </Button>
-                      <Tooltip id="tooltip-top" title={`最終保存時刻 : ${(`00${this.state.lastSaveTime.hour}`).slice(-2)}:${(`00${this.state.lastSaveTime.minute}`).slice(-2)}`} placement="top">
-                        <Button raised onClick={this.saveHot.bind(this)} color="default">
-                          <SaveIcon />
-                         保存
-                        </Button>
-                      </Tooltip>
-                    </div>
-                  </Grid>
+                  <TaskListCtl
+                    lastSaveTime={this.state.lastSaveTime}
+                    saveHot={this.saveHot.bind(this)}
+                    notifiable={this.state.notifiable}
+                    toggleNotifiable={this.toggleNotifiable.bind(this)}
+                  />
                   <Grid item xs={12}>
                     <LinearProgress style={{ visibility: this.state.loading ? 'visible' : 'hidden' }} />
                     <div id="hot" />
---
+++ src/components/TaskListCtl.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import SaveIcon from 'material-ui-icons/Save';
+import AddIcon from 'material-ui-icons/Add';
+import Switch from 'material-ui/Switch';
+import Tooltip from 'material-ui/Tooltip';
+import Grid from 'material-ui/Grid';
+import { FormControlLabel, FormGroup } from 'material-ui/Form';
+
+function addTask() {
+  if (window.hot) {
+    window.hot.alter('insert_row');
+  }
+}
+
+function TaskListCtl(props) {
+  const { lastSaveTime, saveHot, notifiable, toggleNotifiable } = props;
+  return (
+    <Grid container spacing={5}>
+      <Grid item xs={6}>
+        <Typography type="caption" gutterBottom>
+         *終了通知の予約を行うには見積を入力したタスクの開始時刻を入力してください。
+        </Typography>
+        <Typography type="caption" gutterBottom>
+         *通知予約されたタスクの開始時刻に <i className="fa fa-clock-o fa-lg" /> が表示されます。(マウスホバーで予約時刻)
+        </Typography>
+        <Typography type="caption" gutterBottom>
+        *開始時刻を削除、もしくは終了を入力すると終了通知の予約は削除されます。
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <Typography type="caption" gutterBottom>
+      *セル上で右クリックすると現在時刻の入力・行の追加・削除を行えます。
+        </Typography>
+        <Typography type="caption" gutterBottom>
+      *行を選択しドラッグアンドドロップでタスクを入れ替えることができます。
+        </Typography>
+        <Typography type="caption" gutterBottom>
+        *列ヘッダーにマウスホバーすると各列の説明を見ることができます。
+        </Typography>
+      </Grid>
+      <Grid item xs={6}>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Switch
+                disabled={!('Notification' in window)}
+                checked={notifiable}
+                onChange={toggleNotifiable}
+              />
+            }
+            label={`開始したタスクの終了時刻通知${!('Notification' in window) ? '(ブラウザが未対応です。)' : ''}`}
+          />
+        </FormGroup>
+      </Grid>
+      <Grid item xs={6}>
+        <div style={{ textAlign: 'right' }}>
+          <Button raised onClick={addTask} color="default">
+            <AddIcon />
+            追加
+          </Button>
+          <Tooltip id="tooltip-top" title={`最終保存時刻 : ${(`00${lastSaveTime.hour}`).slice(-2)}:${(`00${lastSaveTime.minute}`).slice(-2)}`} placement="top">
+            <Button raised onClick={saveHot} color="default">
+              <SaveIcon />
+             保存
+            </Button>
+          </Tooltip>
+        </div>
+      </Grid>
+    </Grid>);
+}
+
+TaskListCtl.propTypes = {
+  lastSaveTime: PropTypes.shape({
+    hour: PropTypes.number.isRequired,
+    minute: PropTypes.number.isRequired,
+    second: PropTypes.number.isRequired,
+  }).isRequired,
+  saveHot: PropTypes.func.isRequired,
+  notifiable: PropTypes.bool.isRequired,
+  toggleNotifiable: PropTypes.func.isRequired,
+
+};
+
+export default TaskListCtl;
