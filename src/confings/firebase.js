2085d17a43bd1f87d00f67d9c9795152874b7178
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-12-12 00:54:51 +0900

    fix type miss

--- src/confings/firebase.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyASM7G1VKCjQZ8ML48RKxUSuME-Iumz89k',
-  authDomain: 'taskontable.firebaseapp.com',
-  databaseURL: 'https://taskontable.firebaseio.com',
-  projectId: 'taskontable',
-  storageBucket: 'taskontable.appspot.com',
-  messagingSenderId: '27989839492',
-};
