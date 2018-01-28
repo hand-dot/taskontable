0b7c64d3764d48820b8329e393c9f1786176c24e
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-01-29 00:36:44 +0900

    update package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'babel-polyfill';
 import 'font-awesome/css/font-awesome.min.css';
-import polyfill from './polyfill';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-
-polyfill();
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
 registerServiceWorker();
