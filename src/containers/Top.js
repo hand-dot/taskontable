c8cad8825201475fab525033420c60772e59db69
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-03-13 04:36:39 +0900

    change top page

--- src/containers/Top.js
+++ src/containers/Top.js
@@ -7,7 +7,8 @@ import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
 import Button from 'material-ui/Button';
 
-import title from '../images/title_gr.png';
+import titleGr from '../images/title_gr.png';
+import titleWh from '../images/title_wh.png';
 import screencapture from '../images/screencapture.png';
 import scripts from '../images/scripts.png';
 import devices from '../images/devices.png';
@@ -46,19 +47,27 @@ const styles = theme => ({
 });
 
 function Top(props) {
+  fetch('https://api.github.com/repos/hand-dot/taskontable').then(res => res.json()).then((data) => {
+    console.log(data);
+  });
   const { classes } = props;
   return (
     <Grid spacing={0} container alignItems="stretch" justify="center">
       <Grid item xs={12}>
         <Paper className={classes.center} square elevation={0}>
           <div className={classes.content}>
-            <img style={{ margin: '3em' }} src={title} alt="taskontable" height="40" />
-            <Typography color="textSecondary" variant="headline" align="center" style={{ marginBottom: '2em' }}>
-              毎日のワークフローを簡単に作成できるタスクマネージャー
+            <Typography color="textSecondary" variant="display3" align="center" style={{ marginTop: '0.5em', marginBottom: '0.5em' }}>
+              Build Your WorkFlow
+            </Typography>
+            <Typography color="textSecondary" align="center" style={{ marginTop: '0.5em', marginBottom: '0.5em' }}>
+              with
+            </Typography>
+            <img style={{ margin: '2em 2em 2em 1.7em' }} src={titleGr} alt="taskontable" height="40" />
+            <Typography color="textSecondary" align="center" style={{ marginBottom: '2em' }}>
+            毎日のワークフローを簡単に構築できるタスクマネージャー。
             </Typography>
             <Typography color="textSecondary" align="center" style={{ marginBottom: '2em' }}>
-              毎日のワークフローを自動的に作成し、タスクを直列化することで生産性を向上させることができます。<br />
-              終了時刻を常に意識させ、各タスクの開始・終了時刻に通知をするので一日をスムーズに進行することができます。<br />
+            タスクをシーケンシャルにすることで生産性を向上させ一日をスムーズに進行することができます。<br />
             </Typography>
             <Link style={{ marginBottom: '1em', display: 'inline-block' }} className={classes.link} to="/signup"><Button variant="raised" className={classes.button} color="primary" >無料登録して始める</Button></Link>
             <div><Link to="/login">アカウントへのログインはこちら</Link></div>
@@ -68,13 +77,13 @@ function Top(props) {
       <Grid item xs={12}>
         <Paper square className={classes.bgTransparent} elevation={0}>
           <div className={classes.content}>
-            <div style={{ marginBottom: '2em' }} className={classes.textBox}>
-              TaskontableはExcelを扱うようにタスクを管理できるツールです。高速に入力でき、タスクの整理に使う時間をできる限り減らし、実行する時間を増やすことができます。
-            </div>
+            <Typography align="center" style={{ color: '#fff', marginBottom: '2em' }}>
+              TaskontableはExcelを扱うようにタスクを管理できるツールです。<br />高速に入力でき、タスクの整理に使う時間をできる限り減らし、実行する時間を増やすことができます。
+            </Typography>
             <img className={classes.center} style={{ marginBottom: '2em', width: '100%' }} src={screencapture} alt="taskontable" />
-            <div style={{ marginBottom: '2em' }} className={classes.textBox}>
-              どのデバイスでも最新状況をいつでも把握できるよう、保存したテーブルの内容はリアルタイムでアップデートします。複雑な設定は一切必要なく、すぐに使い始められます。
-            </div>
+            <Typography align="center" style={{ color: '#fff', marginBottom: '2em' }}>
+              複雑な設定は一切必要なく、すぐに使い始められます。
+            </Typography>
           </div>
         </Paper>
       </Grid>
@@ -82,10 +91,33 @@ function Top(props) {
         <Paper className={classes.center} square elevation={0}>
           <div className={classes.content}>
             <Typography color="textSecondary" variant="headline" align="center" style={{ marginBottom: '2em' }}>
-              オープンソース
+              Plugin
+            </Typography>
+            <Grid spacing={0} container alignItems="stretch" justify="center">
+              <Grid item xs={12} md={6}>
+                <Typography color="textSecondary" variant="headline" align="center" style={{ marginBottom: '2em' }}>
+                  <img className={classes.center} style={{ marginBottom: '2em', width: '100%', maxWidth: 480 }} src={scripts} alt="taskontable" />
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Typography color="textSecondary" align="center" style={{ marginBottom: '2em' }}>
+                プラグインをインストールしてさまざまなサービスと連携できます。<br />
+              また誰でもプラグインを公開することが可能です。
+                </Typography>
+              </Grid>
+            </Grid>
+          </div>
+        </Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.center} square elevation={0}>
+          <div className={classes.content}>
+            <Typography color="textSecondary" variant="headline" align="center" style={{ marginBottom: '2em' }}>
+              Collaboration
             </Typography>
             <Typography color="textSecondary" align="center" style={{ marginBottom: '2em' }}>
-              ソースコードを<a style={{ margin: '0 .4em' }} href={constants.REPOSITORY_URL} target="_blank">github</a>で公開・配布されています。<br />全てが無料です。
+            ワークフローをチームで共有することが可能です。<br />
+              リアルタイムに進捗状況・予定を把握することができます。
             </Typography>
           </div>
         </Paper>
@@ -94,12 +126,12 @@ function Top(props) {
         <Paper square className={classes.center} elevation={0}>
           <div className={classes.content}>
             <Typography color="textSecondary" variant="headline" align="center" style={{ marginBottom: '2em' }}>
-              あらゆるデバイスに対応
+            Anytime, anywhere
             </Typography>
             <Grid spacing={0} container alignItems="stretch" justify="center">
               <Grid item xs={12} md={6}>
                 <Typography color="textSecondary" align="center" style={{ marginBottom: '2em' }}>
-                  あらゆるデバイスで快適なタスク管理が可能に。いつ、どこにいても最新情報をチェックして素早く対応できます。
+                いつ、どこにいてもあらゆるデバイスで快適なタスク管理が可能です。
                 </Typography>
               </Grid>
               <Grid item xs={12} md={6}>
@@ -115,39 +147,25 @@ function Top(props) {
         <Paper className={classes.center} square elevation={0}>
           <div className={classes.content}>
             <Typography color="textSecondary" variant="headline" align="center" style={{ marginBottom: '2em' }}>
-            プラグイン
-            </Typography>
-            <Grid spacing={0} container alignItems="stretch" justify="center">
-              <Grid item xs={12} md={6}>
-                <Typography color="textSecondary" variant="headline" align="center" style={{ marginBottom: '2em' }}>
-                  <img className={classes.center} style={{ marginBottom: '2em', width: '100%', maxWidth: 480 }} src={scripts} alt="taskontable" />
-                </Typography>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Typography color="textSecondary" align="center" style={{ marginBottom: '2em' }}>
-                プラグインをインストールしてさまざまなサービスと連携することが可能です。<br />
-              また誰でも作成したスクリプトを公開することが可能です。
-                </Typography>
-              </Grid>
-            </Grid>
-          </div>
-        </Paper>
-      </Grid>
-      <Grid item xs={12}>
-        <Paper className={classes.center} square elevation={0}>
-          <div className={classes.content}>
-            <Typography color="textSecondary" variant="headline" align="center" style={{ marginBottom: '2em' }}>
-              コラボレーション
+              Open Source
             </Typography>
             <Typography color="textSecondary" align="center" style={{ marginBottom: '2em' }}>
-            ワークフローをチームで共有することが可能です。<br />
-              リアルタイムに進捗状況・予定を簡単に把握することができます。
+              ソースコードを<a style={{ margin: '0 .4em' }} href={constants.REPOSITORY_URL} target="_blank">github</a>で公開・配布されています。<br />全てが無料です。
             </Typography>
           </div>
         </Paper>
       </Grid>
       <Grid item xs={12}>
         <Paper square className={classes.bgTransparent} elevation={0}>
+          <div className={classes.content}>
+            <Typography color="textSecondary" variant="display3" align="center" style={{ marginTop: '0.5em', marginBottom: '0.5em' }}>
+              Build Your WorkFlow
+            </Typography>
+            <Typography color="textSecondary" align="center" style={{ marginTop: '0.5em', marginBottom: '0.5em' }}>
+              with
+            </Typography>
+            <img style={{ margin: '2em auto', display: 'block' }} src={titleWh} alt="taskontable" height="40" />
+          </div>
           <div className={classes.content}>
             <Grid spacing={0} container alignItems="stretch" justify="center">
               <Grid item xs={4} style={{ marginBottom: '2em' }} className={classes.center}>
