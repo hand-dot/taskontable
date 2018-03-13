03c19edc223d71c15ef88b1d4b78dc7d943438d9
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-03-13 23:53:05 +0900

    ref #259

--- src/exampleDatas/exampleExportScript.js
+++ src/exampleDatas/exampleExportScript.js
@@ -1,5 +1,12 @@
 /* eslint no-param-reassign: ["error", { "props": false }] */
 export default function exportScript(e) {
-  // TODO githubに連携する
+  const YOUR_TOKEN = 'YourPersonalAccessTokens';
+  e.data.filter(data => data.title.startsWith('@github ') && data.startTime && data.endTime).forEach((data) => {
+    const URL = `${data.memo.replace(/github.com/i, 'api.github.com/repos')}?access_token=${YOUR_TOKEN}`;
+    fetch(URL, {
+      method: 'PATCH',
+      body: JSON.stringify({ state: 'closed' }),
+    });
+  });
   postMessage(e.data);
 }
--- src/exampleDatas/exampleImportScript.js
+++ src/exampleDatas/exampleImportScript.js
@@ -1,5 +1,10 @@
 /* eslint no-param-reassign: ["error", { "props": false }] */
 export default function importScript(e) {
-  // TODO githubからissueを所得する
-  postMessage(e.data);
+  const YOUR_TOKEN = 'YourPersonalAccessTokens';
+  const YOUR_URL = `https://api.github.com/repos/hand-dot/taskontable/issues?state=open&labels=Status%3A+progress&access_token=${YOUR_TOKEN}`;
+  fetch(YOUR_URL).then(res => res.json()).then((datas) => {
+    const issues = datas.map(data => ({ id: data.id.toString(), title: `@github ${data.title}`, estimate: '', startTime: '', endTime: '', memo: `https://github.com/hand-dot/taskontable/issues/${data.number}` }));
+    const tasksIds = Array.from(new Set(e.data.map(task => task.id)));
+    postMessage(e.data.concat(issues.filter(data => !tasksIds.includes(data.id))));
+  });
 }
