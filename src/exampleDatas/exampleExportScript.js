d77046d98c9fc3a68df50f839cc560e8dbe1314a
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-03-14 00:37:43 +0900

    exportscript minor fix

--- src/exampleDatas/exampleExportScript.js
+++ src/exampleDatas/exampleExportScript.js
@@ -1,12 +1,12 @@
 /* eslint no-param-reassign: ["error", { "props": false }] */
 export default function exportScript(e) {
   const YOUR_TOKEN = 'YourPersonalAccessTokens';
-  e.data.filter(data => data.title.startsWith('@github ') && data.startTime && data.endTime).forEach((data) => {
-    const URL = `${data.memo.replace(/github.com/i, 'api.github.com/repos')}?access_token=${YOUR_TOKEN}`;
-    fetch(URL, {
-      method: 'PATCH',
-      body: JSON.stringify({ state: 'closed' }),
-    });
+  e.data.forEach((data) => {
+    if (data.title.startsWith('@github ')) {
+      const URL = `${data.memo.replace(/github.com/i, 'api.github.com/repos')}?access_token=${YOUR_TOKEN}`;
+      const state = data.startTime && data.endTime ? 'closed' : 'open';
+      fetch(URL, { method: 'PATCH', body: JSON.stringify({ state }) });
+    }
   });
   postMessage(e.data);
 }
