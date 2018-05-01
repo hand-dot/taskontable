0bd0d858ec67d6e8f2204ae8b681372cf5df183c
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-05-01 15:35:04 +0900

    minor fix cloud functions

--- functions/index.js
+++ functions/index.js
@@ -5,41 +5,37 @@ const os = require('os');
 const fs = require('fs');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
 admin.initializeApp();
 
-
 // Checks if uploaded images are flagged as Adult or Violence and if so blurs them.
-exports.blurOffensiveImages = functions.storage.object().onFinalize(object => {
-    const image = {
-        source: { imageUri: `gs://${object.bucket}/${object.name}` },
-    };
-    return blurImage(object.name, object.bucket);
-});
+exports.blurOffensiveImages = functions.storage.object().onFinalize((object) => {
+  const filePath = object.name;
+  const bucketName = object.bucket;
+  const tempLocalFile = path.join(os.tmpdir(), path.basename(filePath));
+  const messageId = filePath.split(path.sep)[1];
+  const bucket = gcs.bucket(bucketName);
 
-// Blurs the given image located in the given bucket using ImageMagick.
-function blurImage(filePath, bucketName) {
-    const tempLocalFile = path.join(os.tmpdir(), path.basename(filePath));
-    const messageId = filePath.split(path.sep)[1];
-    const bucket = gcs.bucket(bucketName);
-
-    // Download file from bucket.
-    return bucket.file(filePath).download({ destination: tempLocalFile }).then(() => {
-        console.log('Image has been downloaded to', tempLocalFile);
-        // Blur the image using ImageMagick.
-        return spawn('convert', [tempLocalFile, '-channel', 'RGBA', '-blur', '0x24', tempLocalFile]);
-    }).then(() => {
-        console.log('Image has been blurred');
-        // Uploading the Blurred image back into the bucket.
-        return bucket.upload(tempLocalFile, { destination: filePath });
-    }).then(() => {
-        console.log('Blurred image has been uploaded to', filePath);
-        // Deleting the local file to free up disk space.
-        fs.unlinkSync(tempLocalFile);
-        console.log('Deleted local file.');
-        // Indicate that the message has been moderated.
-        return admin.database().ref(`/messages/${messageId}`).update({ moderated: true });
-    }).then(() => {
-        console.log('Marked the image as moderated in the database.');
-        return null;
+  // Download file from bucket.
+  return bucket.file(filePath).download({ destination: tempLocalFile }).then(() => {
+    console.log('Image has been downloaded to', tempLocalFile);
+    // Blur the image using ImageMagick.
+    return spawn('convert', [tempLocalFile, '-channel', 'RGBA', '-blur', '0x24', tempLocalFile]);
+  }).then(() => {
+    console.log('Image has been blurred');
+    // Uploading the Blurred image back into the bucket.
+    return bucket.upload(tempLocalFile, { destination: filePath });
+  })
+    .then(() => {
+      console.log('Blurred image has been uploaded to', filePath);
+      // Deleting the local file to free up disk space.
+      fs.unlinkSync(tempLocalFile);
+      console.log('Deleted local file.');
+      // Indicate that the message has been moderated.
+      return admin.database().ref(`/messages/${messageId}`).update({ moderated: true });
+    })
+    .then(() => {
+      console.log('Marked the image as moderated in the database.');
+      return null;
     });
-}
+});
