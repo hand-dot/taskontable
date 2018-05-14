c7f518d0f7fea65e3feac83f4b22d08106ec88e1
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-05-15 05:22:57 +0900

    change font-awesome cdn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import CssBaselines from 'material-ui/CssBaseline';
 import { MuiThemeProvider } from 'material-ui/styles';
-import 'font-awesome/css/font-awesome.min.css';
 import ErrorBoundary from './containers/ErrorBoundary';
 import App from './containers/App';
 import WithTracker from './containers/WithTracker';
