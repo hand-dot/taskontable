af8fa781f301078ae391bf35a1c5f86525e867c6
hand-dot <kyouhei.fukuda0729@gmail.com>
2018-09-15 01:02:45 +0900

    fix bug

--- src/components/TaskProcessing.js
+++ src/components/TaskProcessing.js
@@ -29,14 +29,10 @@ function getTitle(processingTask, defaultTitle, isShortTitle) {
   const actuallyMinute = util.getTimeDiffMinute(processingTask.startTime, processingTask.now);
   let title = '';
   if (processingTask.id) {
-    title = isShortTitle ? `${(processingTask.title.length < 18 ? processingTask.title
-      || '' : `${processingTask.title.substring(0, 15)}...`)
-      || i18n.t('common.anonymousTask')}` : processingTask.title
-      || i18n.t('common.anonymousTask');
+    title = isShortTitle ? `${(processingTask.title.length < 18 ? processingTask.title || '' : `${processingTask.title.substring(0, 15)}...`) || i18n.t('common.anonymousTask')}` : processingTask.title || i18n.t('common.anonymousTask');
     if (processingTask.estimate) {
       const isOver = actuallyMinute >= processingTask.estimate;
-      if (processingTask.estimate - actuallyMinute === 1
-         || actuallyMinute - processingTask.estimate === 0) {
+      if (processingTask.estimate - actuallyMinute === 1 || actuallyMinute - processingTask.estimate === 0) {
         const sec = moment(processingTask.now, 'HH:mm:ss').format('s');
         title = `${i18n.t(`worksheet.tableCtl.taskProcessing.${isOver ? 'over_target' : 'remaining_target'}`, { target: (isOver ? sec : 60 - sec) + i18n.t('common.sec') })} - ${title}`;
       } else if (isOver) {
@@ -84,16 +80,15 @@ class TaskProcessing extends Component {
     const { date } = this.props;
     if (isMobile) return;
     if (this.bindProcessingTaskIntervalID) clearInterval(this.bindProcessingTaskIntervalID);
-    const prcesTask = tasksUtil.getSortedTasks(tasks).find(task => task.startTime && task.endTime === '');
-    if (util.isToday(date) && prcesTask) {
-      this.bindprcesTaskIntervalID = setInterval(() => {
+    const processingTask = tasksUtil.getSortedTasks(tasks).find(task => task.startTime && task.endTime === '');
+    if (util.isToday(date) && processingTask) {
+      this.bindProcessingTaskIntervalID = setInterval(() => {
         const now = moment();
-        const newTimeDiffMinute = util.getTimeDiffMinute(prcesTask.startTime,
-          now.format(constants.TIMEFMT));
+        const newTimeDiffMinute = util.getTimeDiffMinute(processingTask.startTime, now.format(constants.TIMEFMT));
         if (newTimeDiffMinute >= 0) {
-          prcesTask.now = now.format('HH:mm:ss');
-          this.setState({ processingTask: util.setIdIfNotExist(prcesTask) });
-        } else if (!util.equal(prcesTask, getProcessingTaskSchema)) {
+          processingTask.now = now.format('HH:mm:ss');
+          this.setState({ processingTask: util.setIdIfNotExist(processingTask) });
+        } else if (!util.equal(processingTask, getProcessingTaskSchema)) {
           this.setState({ processingTask: getProcessingTaskSchema() });
         }
         this.oldTimeDiffMinute = newTimeDiffMinute;
@@ -101,9 +96,7 @@ class TaskProcessing extends Component {
     } else {
       this.bindProcessingTaskIntervalID = '';
       document.title = constants.TITLE;
-      if (!util.equal(prcesTask, processingTaskSchema)) {
-        this.setState({ processingTask: getProcessingTaskSchema() });
-      }
+      if (!util.equal(processingTask, processingTaskSchema)) this.setState({ processingTask: getProcessingTaskSchema() });
     }
   }
 
@@ -135,8 +128,7 @@ class TaskProcessing extends Component {
           style={{
             fontSize: 15,
             marginRight: theme.spacing.unit,
-            color: processingTask.id
-              ? constants.brandColor.base.RED : constants.brandColor.base.GREY,
+            color: processingTask.id ? constants.brandColor.base.RED : constants.brandColor.base.GREY,
             animation: processingTask.id ? 'heartbeat 2s infinite' : '',
           }}
         />
@@ -145,11 +137,7 @@ class TaskProcessing extends Component {
             {getTitle(processingTask, i18n.t('worksheet.tableCtl.taskProcessing.thereAreNoStartingTasks'), true)}
           </Typography>
           <LinearProgress
-            classes={{
-              root: classes.progress,
-              barColorPrimary: classes[color],
-              colorPrimary: classes.grey,
-            }}
+            classes={{ root: classes.progress, barColorPrimary: classes[color], colorPrimary: classes.grey }}
             variant="determinate"
             value={100 - remainPercent <= 0 ? 100 : 100 - remainPercent}
           />
